518653e04cc1263cf833403cb08e01c32e51ed00
Suhas SG <jargnar@gmail.com>
2015-02-23 12:59:47 +0530

    Attempt sum of multiples with elixirs h,t

--- elixir/sum_of_multiples.exs
+++ elixir/sum_of_multiples.exs
@@ -1,5 +1,25 @@
+# Using Enums
 sum = Enum.sum Enum.filter(
     Enum.to_list(1..999),
     fn(x) -> rem(x, 3) == 0 or rem(x, 5) == 0 end
   )
-IO.puts sum
+
+# Using some elixir h|t pattern matching
+defmodule Multiples do
+  def sum([]) do
+    0
+  end
+  def sum([h|t]) do
+    if rem(h, 3) == 0 or rem(h, 5) == 0 do
+      h + sum(t)
+    else
+      sum(t)
+    end
+  end
+end
+
+# if method 1 == method 2
+# print the result (either one)
+if sum == Multiples.sum(Enum.to_list 1..999) do
+  IO.puts sum
+end
