2ec3f273bed4e52499f29f00886b9c30f1d4bb82
Suhas SG <jargnar@gmail.com>
2015-02-23 20:38:43 +0530

    ENH: solve sum of multiples using fn capturing and pipes

--- sum_of_multiples.exs
+++ sum_of_multiples.exs
@@ -1,10 +1,14 @@
-# Using Enums
-sum = Enum.sum Enum.filter(
-    Enum.to_list(1..999),
+############################################
+# Using Enums                              #
+############################################
+sum1 = Enum.sum Enum.filter(
+    1..999,
     fn(x) -> rem(x, 3) == 0 or rem(x, 5) == 0 end
   )
 
-# Using h|t pattern matching and recursion
+############################################
+# Using h|t pattern matching and recursion #
+############################################
 defmodule Multiples do
   def sum([]) do
     0
@@ -18,8 +22,18 @@ defmodule Multiples do
   end
 end
 
-# if method 1 == method 2
-# print the result (either one)
-if sum == Multiples.sum(Enum.to_list 1..999) do
-  IO.puts sum
+sum2 = Multiples.sum Enum.to_list 1..999
+
+###########################################
+# Using Enums, Fn Capturing and Pipes     #
+###########################################
+divs3or5? = &(rem(&1, 3) == 0 or rem(&1, 5) == 0)
+sum3 = 1..999
+  |> Enum.filter(divs3or5?)
+  |> Enum.sum
+
+
+# How do I better check if all the sums are equal?
+if (sum1 == sum2) and (sum2 == sum3) and (sum3 == sum1) do
+  IO.puts sum1
 end
