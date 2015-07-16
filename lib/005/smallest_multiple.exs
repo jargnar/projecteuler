a8740ab6a66acc01081dc79f690bddb8e7152020
Suhas SG <jargnar@gmail.com>
2015-07-16 17:16:31 +0530

    STY: indentation changed to 2 spaces

--- lib/005/smallest_multiple.exs
+++ lib/005/smallest_multiple.exs
@@ -1,7 +1,7 @@
 defmodule Multiple do
-	def gcd(a, 0), do: a
-	def gcd(a, b), do: gcd(b, rem(a, b))
-	def lcm(a, b), do: a * div(b, gcd(a, b))
+  def gcd(a, 0), do: a
+  def gcd(a, b), do: gcd(b, rem(a, b))
+  def lcm(a, b), do: a * div(b, gcd(a, b))
 end
 
 IO.puts Enum.reduce(1..20, &Multiple.lcm/2)
