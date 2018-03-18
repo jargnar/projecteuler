80d4318ac87119b008af4f5f4ca6f17d9a712c66
Suhas SG <jargnar@gmail.com>
2018-03-18 20:57:31 +0100

    Enh: condense functions

--- go/1.go
+++ go/1.go
@@ -5,18 +5,15 @@ import (
 )
 
 // finds the summation of arithmetic progression
-// with N elements, first element a, and difference d
-func sumToN(a int, n int, d int) int {
-	return (n / 2) * ((2 * a) + (n-1)*d)
-}
-
-// calls sumToN with approppriate numbers
-func multipleSum(a int, lim int) int {
-	return sumToN(a, (lim-1)/a, a)
+// upto lim, first element a, and difference a
+func sumOfMultiples(a int, lim int) int {
+	var n int
+	n = (lim - 1) / a
+	return (n / 2) * ((2 * a) + (n-1)*a)
 }
 
 func main() {
 	var sum int
-	sum = multipleSum(3, 1000) + multipleSum(5, 1000) - multipleSum(15, 1000)
+	sum = sumOfMultiples(3, 1000) + sumOfMultiples(5, 1000) - sumOfMultiples(15, 1000)
 	fmt.Println(sum)
 }
