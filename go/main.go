a579e6ed39cc9c2b58dd81ce9e01a4feb6e9c07d
Suhas SG <jargnar@gmail.com>
2018-03-24 15:37:01 +0100

    Use primefactors in problem 3

--- go/main.go
+++ go/main.go
@@ -1,11 +1,8 @@
 package main
 
-import "fmt"
-
 func main() {
 	multiples()          // 1
 	fibo()               // 2
 	largestPrimeFactor() // 3
 	palindromeProduct()  // 4
-	fmt.Println(primefactors(600851475143))
 }
